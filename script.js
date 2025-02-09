942d842978889869a5be454ff2c3150153623c9c
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-02-09 22:43:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const codes=document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code,index)=>{
+	code.addEventListener("input",(e)=>{
+		if(!/^\d$/.test(e.target.value)){
+			e.target.value="";
+			return;
+		}
+		if(e.target.value!==""&& index<codes.length-1)
+		{
+			codes[index+1].focus();
+		}
+	});
+	code.addEventListener("keydown",(e)=>{
+		if(e.key==="Backspace"&&e.target.value==="")
+		{
+			codes[index-1].focus();
+		}
+	});
+});
+
